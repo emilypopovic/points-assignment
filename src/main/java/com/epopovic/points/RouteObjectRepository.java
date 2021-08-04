7b34a9c877a2d0c5ea2039aeb5f461a1d0e750d0
Emily Popovic <epopovic@uwo.ca>
2021-08-03 22:46:24 -0400

    validate successful routes

--- src/main/java/com/epopovic/points/RouteObjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epopovic.points;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface RouteObjectRepository extends JpaRepository<RouteObject, Long> {
-//    RouteObject findById(String id);
-//}
--- src/main/java/com/epopovic/points/RouteValidationService.java
+++ src/main/java/com/epopovic/points/RouteValidationService.java
@@ -8,47 +8,59 @@ public class RouteValidationService {
     public RouteValidationService() {}
 
     static String validateRoute(RouteResponse routeResponse) {
-
         assert routeResponse != null;
         List<TrackObject> tracksObject = routeResponse.getRoute().getTrack();
         List<TravelLogObject> travelLogsObject = routeResponse.getRoute().getTravelLog();
 
-        HashMap<Integer, String[]> obstacleKeyMap = new HashMap<>();
+        //determine the last position of both routes
+        int finalTrackIndex = tracksObject.size()>0 ? tracksObject.get(tracksObject.size() - 1).getPosition() : 0;
+        int finalTravelLogIndex = travelLogsObject.size()>0 ? travelLogsObject.get(travelLogsObject.size() - 1).getPosition() : 0;
+        int finalIndex = Math.max(finalTrackIndex, finalTravelLogIndex);
 
-        //check for empty travel log with obstacles
-        //for each entry in obstacleMap check if there is an obstacle in start lane "b"
-        if(travelLogsObject.isEmpty() && !tracksObject.isEmpty()) {
-            for (TrackObject track: tracksObject) {
-                boolean result = Arrays.asList(track.getObstacles()).contains("b");
-                if (result) {
-                    return "{\"status\": \"error\", \"position\": " + track.getPosition() + "}";
-                }
-                obstacleKeyMap.put(track.getPosition(), track.getObstacles());
-            }
-            return "{\"status\": \"error\", \"position\": \"what\"}";
-
+        //map the position to the obstacles in that position
+        //contains position and obstacles list
+        Map<Integer, String[]> obstacleKeyMap = new HashMap<>();
+        for (TrackObject track: tracksObject) {
+            obstacleKeyMap.put(track.getPosition(), track.getObstacles());
         }
-        else if(!travelLogsObject.isEmpty()) {
-            //determine where the car is travelling
-            //laneChange and position
-            int currentLaneIndex = 1;
-            int currentPosition = 0;
 
-            for (TravelLogObject travelLog: travelLogsObject) {
-                if(travelLog.getLaneChange().equals("left")) {
-                    currentLaneIndex--;
+        //map the position to the lane change for that position
+        //contains position and laneChange
+        Map<Integer, String> travelLogKeyMap = new HashMap<>();
+        for (TravelLogObject travelLog: travelLogsObject) {
+            travelLogKeyMap.put(travelLog.getPosition(), travelLog.getLaneChange());
+        }
+
+        //need to loop through success with objects to check if you go around
+        //for each position, check obstacle key map for obstacle, if none continue
+        char currentLane = 'b';
+        for(int i=0; i <= finalIndex; i++) {
+
+            //if that position has a travel log entry
+            //travelLog:[.....]
+            if(travelLogKeyMap.containsKey(i)) {
+                String laneChange = travelLogKeyMap.get(i);
+                if(laneChange.equals("left")) {
+                    currentLane--;
                 }
                 else {
-                    currentLaneIndex++;
+                    currentLane++;
                 }
-                if(currentLaneIndex < 0 || currentLaneIndex > 2) {
-                    return "{\"status\": \"error\", \"position\": " + currentPosition + "}";
+                if(currentLane < 'a' || currentLane > 'c') {
+                    return "{\"status\": \"error\", \"position\": " + i + "}";
                 }
-                currentPosition = travelLog.getPosition();
+            }
+            //if that position has an obstacle entry
+            //track:[.....]
+            if(obstacleKeyMap.containsKey(i)) {
+                String[] obstacles = obstacleKeyMap.get(i);
+
+                //check if there is an obstacle in the current lane
+                if (Arrays.asList(obstacles).contains(String.valueOf(currentLane))) {
+                    return "{\"status\": \"error\", \"position\": " + i + "}";
+                }
             }
         }
-
         return "{\"status\": \"success\"}";
     }
-
 }
